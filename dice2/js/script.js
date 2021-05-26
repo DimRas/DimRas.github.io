5189644dde5cc4483d270c561c937548e21c6c67
DimRas <dima86R@gmail.com>
2021-05-26 03:32:39 +0300

    initial commit

--- dice2/js/script.js
+++ dice2/js/script.js
@@ -60,7 +60,7 @@ const btnRollFun = btnRoll.addEventListener('click', function() {
     
     //2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `img/dice-${dice}.png`;
     
     //3.Check for rolled 1; if true, switch to next player
     if(dice !== 1) {
